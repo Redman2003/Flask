d99e7bfe4cd998fd40288c2012817dbb654a4817
Jacob <118228102+Redman2003@users.noreply.github.com>
2024-05-13 23:27:52 -0400

    Update boombox.py

--- boombox.py
+++ boombox.py
@@ -104,4 +104,4 @@ music_init_db(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE)
 
 # driver
 if __name__ == "__main__":
-    app.run(debug=True ,port = 5000 , host = '0.0.0.0'))
+    app.run(debug=True ,port = 5000 , host = '0.0.0.0')
